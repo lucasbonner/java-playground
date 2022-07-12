5b115febb3ffdc642b72cb7c0086b7cf6dea9ec1
Lucas <lucasbonner@protonmail.com>
2022-07-12 12:33:08 -0500

    adds some exercises from intro to java

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,6 +3,16 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        if (args.length != 2) {
+            System.out.println("You must pass two arguments.");
+            System.exit(1);
+        }
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println(a + b);
     }
+
 }
